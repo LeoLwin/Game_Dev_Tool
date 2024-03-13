624e4e05d42e6744160225555a350a28a88ef31c
KHL <phoekaung.3819@gmail.com>
2024-03-13 15:07:14 +0630

    urrl_Json_Response

--- Controller/userController.js
+++ Controller/userController.js
@@ -3,7 +3,8 @@ const User = require("../models/userModel");
 const googleLogin = async (req, res) => {
   try {
     const url = await User.googleLogin();
-    res.redirect(url);
+    // res.redirect(url);
+    res.status(200).json(url);
   } catch (error) {
     console.error("Error in Google login:", error);
     res.status(500).json({ message: error.message });
